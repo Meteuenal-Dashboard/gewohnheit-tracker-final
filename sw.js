e70277faa156c195a7cc2cac65d6dcc045c7e7e0
Meteuenal-Dashboard <meteuenal@gmail.com>
2026-04-15 18:35:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))); self.clients.claim(); });
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', (e) => {
+    // Network-only - Niemals den alten Code cachen!
+    e.respondWith(fetch(e.request));
+});
